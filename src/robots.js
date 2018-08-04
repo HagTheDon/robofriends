b9bc2c8e1dfc841ba7473f529d3f85d7b0934c04
HagTheDon <donald.haguma@gmail.com>
2018-08-04 09:56:36 +0900

    Robofriends first commit app

---
+++ src/robots.js
@@ -0,0 +1,34 @@
+
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Mark Sanders',
+	username: 'Mark',
+	email: 'Mark@boring.com'
+},
+{
+	id: 3,
+	name: 'Zi German',
+	username: 'German',
+	email: 'german@thaish.biz'
+},
+{
+	id: 4,
+	name: 'Madness',
+	username: 'Awesomest',
+	email: 'awesome@nyc.dif'
+},
+{
+	id: 5,
+	name: 'Leanne Yup',
+	username: 'Yup',
+	email: 'yup@gmail.com'
+},
+
+]
